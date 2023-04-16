4a0ba015ed6f6226e7a757d40ea7fc63e97648de
danielatego <danielatego75@gmail.com>
2023-04-16 20:15:34 +0300

    Java Script Reminder

--- 5.js
+++ 5.js
@@ -630,3 +630,24 @@
 
 // }
 // console.log(chainToSwitch(7))
+
+//Returning Boolean Values from Functions
+
+// function isLess(a,b){
+
+//     return  (a < b)
+// }
+// console.log(isLess(10,15));
+
+//Returning Early Pattern from Functions
+
+
+// function abTest(a,b){
+//     if (a<0 || b<0){
+//         return undefined;
+//     }
+//     return Math.round(Math.pow(Math.sqrt(a)+ Math.sqrt(b),2));
+// }
+// console.log(abTest(2,-2));
+
+//counting cards

820fef7ef15692f4d14b00515d173dfbb9d117f6
danielatego <danielatego75@gmail.com>
2023-04-24 12:14:34 +0300

    destructuring assignment witn rest of operator

--- 5.js
+++ 5.js
@@ -1307,15 +1307,27 @@
 
 //Use Destructuring Assignment to Assign Variables from Arrays
 
-const [z,x, , , y] = [7,2,3,4,5,6];
+// const [z,x, , , y] = [7,2,3,4,5,6];
 
-console.log(z,x,y);
+// console.log(z,x,y);
 
-let a = 8, b = 6;
+// let a = 8, b = 6;
 
-(() => {
-    "use strict";
-    [a,b] = [b,a];
-}) ();
-console.log(a);
-console.log(b);
+// (() => {
+//     "use strict";
+//     [a,b] = [b,a];
+// }) ();
+// console.log(a);
+// console.log(b);
+
+//Use Destructuring Assignment with Rest Operator
+
+const source = [1,2,3,4,5,6,7,8,9,10];
+function removeFirstTwo (list){
+    const [k,l, ...arr] = list;
+    
+    return arr;
+}
+const arr = removeFirstTwo(source);
+console.log(arr);
+console.log(source);

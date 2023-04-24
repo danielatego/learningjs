4e6c9a7e39ad5170e167d2d0874171b9b57cdc85
danielatego <danielatego75@gmail.com>
2023-04-24 10:47:27 +0300

    higher order arrow functions

--- 5.js
+++ 5.js
@@ -1216,7 +1216,16 @@
 
 //Write Arrow Functions with Parameters
 
-var myConcat = (arr1,arr2)=>{
-    return arr1.concat(arr2)
-}
-console.log(myConcat([1,3],[2,6,4]))
+//  var myConcat = (arr1,arr2)=>arr1.concat(arr2)
+//  console.log(myConcat([1,3],[2,6,4]))
+
+//Write Higher Order Arrow Functions
+
+// const realNumberArray = [ 4, 5.6,-9.8,3.14,42,6,8.34,-2];
+// const squareList = (arr) => {
+  
+//     const squaredIntegers = arr.filter(num => Number.isInteger(num)&& num>0).map(x => x*x);
+//     return squaredIntegers;
+// }
+// const squaredIntegers = squareList(realNumberArray)
+// console.log(squaredIntegers);

2a2f37c16d6262343aa6643c94c3fa0f2d11a587
danielatego <danielatego75@gmail.com>
2023-04-25 13:14:46 +0300

    default export

--- 7.js
+++ 7.js
@@ -3,3 +3,8 @@ const cap = y.capitalizeString('hi there');
 console.log(cap);
 console.log(y.foo)
 //use export to Reuse a code Block
+// Create an Export Fallback with export default
+export default function subtract(x,y) {return x-y;}
+//import a default export
+import subtract from "./6.js";//the difference with other
+//imports is absence of curly braces between the imported function

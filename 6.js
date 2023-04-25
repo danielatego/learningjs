560b2881e8f40b8128d93b98084c97daaef67773
danielatego <danielatego75@gmail.com>
2023-04-25 12:54:27 +0300

    to export and reuse code block and variables

--- 6.js
+++ 6.js
@@ -1,4 +1,13 @@
 //Understand the Difference Between import and require
 //for an export between modules to be successfull 
 //a package.json file is required with the type set to module
-export const capitalizeString = str => str.toUpperCase()
+// export const capitalizeString = str => str.toUpperCase()
+
+//use export to Reuse a code Block
+const capitalizeString = (string)=>{
+    return string.charAt(0).toUpperCase() + string.slice(1);
+} 
+export {capitalizeString};
+
+export const foo = "bar";
+export const bar = 'foo';
--- 7.js
+++ 7.js
@@ -1,4 +1,6 @@
 import { capitalizeString } from "./6.js";
 const cap = capitalizeString('hi there');
-
+import { foo } from "./6.js";
 console.log(cap);
+console.log(foo)
+//use export to Reuse a code Block
